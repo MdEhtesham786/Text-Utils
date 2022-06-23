7ea4aa8e220bf51c1a128676543a7d179fc12bb2
Ehtesham <mdehteshamshaikh1@gmail.com>
2022-06-24 00:29:13 +0530

    to

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,27 +4,27 @@ export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} style={props.navbarStyle}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">{props.title}</Link>
+                <Link className="navbar-brand" to="/Text-Utils/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent"  >
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <Link className="nav-link active" aria-current="page" to="/Text-Utils/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">{props.link}</Link>
+                            <Link className="nav-link" to="/Text-Utils/about">{props.link}</Link>
                         </li>
                         <li className="nav-item dropdown">
                             <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
                             </Link>
                             <ul className={`dropdown-menu bg-black bg-${props.bgColor}`} aria-labelledby="navbarDropdown" >
-                                <li><Link className={`dropdown-item text-${props.txtColor}`} to="/">Action</Link></li>
-                                <li><Link className={`dropdown-item text-${props.txtColor}`} to="/">Another action</Link></li>
+                                <li><Link className={`dropdown-item text-${props.txtColor}`} to="/Text-Utils/">Action</Link></li>
+                                <li><Link className={`dropdown-item text-${props.txtColor}`} to="/Text-Utils/">Another action</Link></li>
                                 <li><hr className={`dropdown-divider text-${props.txtColor}`} /></li>
-                                <li><Link className={`dropdown-item text-${props.txtColor}`} to="/">Something else here</Link></li>
+                                <li><Link className={`dropdown-item text-${props.txtColor}`} to="/Text-Utils/">Something else here</Link></li>
                             </ul>
                         </li>
                     </ul>
